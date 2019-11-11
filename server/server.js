eb57ab57b089371f4eae8c277537a297d4f21baa
Leonel Curti <leonelcurti@gmail.com>
2019-11-10 22:09:16 -0300

    Adding search products by conditions

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
 const { User } = require("./models/user");
+const { Product } = require("./models/product");
 const { Brand } = require("./models/brand");
+const { Wood } = require("./models/wood");
 const { auth } = require("./middleware/auth");
 const { admin } = require("./middleware/admin");
 
@@ -42,6 +44,95 @@ app.get("/api/product/brands", (req, res) => {
   Brand.find({}, (err, brands) => {
     if (err) return res.status(400).send(err);
     res.status(200).send(brands);
+  });
+});
+
+//-------------------------------
+//-----WOODS---------------------
+//-------------------------------
+
+app.post("/api/product/wood", auth, admin, (req, res) => {
+  const wood = new Wood(req.body);
+
+  wood.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      wood: doc
+    });
+  });
+});
+
+app.get("/api/product/woods", (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(woods);
+  });
+});
+
+//-------------------------------
+//-----PRODUCTS------------------
+//-------------------------------
+
+
+//BY ARRIVAL   
+//articles?sortBy=createdAt&order=desc&limit=4
+
+//BY SELL
+//articles?sortBy=sold&desc&limit=4
+
+app.get('/api/product/articles', (req, res)=>{
+  let order = req.query.order ? req.query.order :'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy :'_id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+  
+  Product
+    .find()
+    .populate('brand')
+    .populate('wood')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, docs)=>{
+      if(err) return res.status(400).send(err)
+      return res.status(200).send(docs)
+    })
+})
+
+//BY ID
+//article?id=sdfsfs,sdfsdf,fghfgh&type=array
+//article?id=sdfsfs&type=single
+
+app.get("/api/product/articles_by_id", (req, res) => {
+  let type = req.query.type;
+  let items = req.query.id;
+
+  if (type === "array") {
+    let ids = items.split(",");
+    items = [];
+    items = ids.map(item => {
+      return mongoose.Types.ObjectId(item);
+    });
+  }
+
+  Product
+    .find({ _id: { $in: items } })
+    .populate('brand')
+    .populate('wood')
+    .exec((err, docs)=>{
+      if(err) return res.status(400).send(err)
+      return res.status(200).send(docs)
+    })
+});
+
+app.post("/api/product/article", auth, admin, (req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      product: doc
+    });
   });
 });
 
