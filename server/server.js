22f36ae05c88de874b86575a8653283933d7087e
Leonel Curti <leonelcurti@gmail.com>
2020-01-27 00:06:25 -0300

    Server config

--- server/server.js
+++ server/server.js
@@ -232,8 +232,8 @@ app.get("/api/users/removeFromCart", auth, (req, res) => {
 //DEFAULT ROUTE
 
 if (process.env.NODE_ENV === "production") {
-  app.get("/*", (req, res) => {
-    res.sendfile(path.resolve(__dirname, "../client", "build", "index.html"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
